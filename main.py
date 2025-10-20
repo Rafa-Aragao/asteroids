4c3510dc8b4c2fe6a57843f17d45a639592262b5
Rafa-Aragao <rafa.aragao05@gmail.com>
2025-10-19 23:42:50 -0300

    Added player

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -11,7 +16,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
